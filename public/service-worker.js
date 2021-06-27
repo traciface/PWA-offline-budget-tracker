09ca3225c65ad7cd5b1b255a76bf500bfcecb2ed
Traci <traci.face@gmail.com>
2021-06-26 22:58:48 -0400

    service worker from class work

---
+++ public/service-worker.js
@@ -0,0 +1,71 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/js/db.js",
+    "/assets/images/icons/icon-192x192.png",
+    "/assets/images/icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+  ];
+  
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (evt) => {
+    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(() => {
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
